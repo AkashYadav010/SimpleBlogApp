97a7f80c07c9bfe8f3655541a77d1765c7ed3784
AkashYadav010 <mryadav.akash@gmail.com>
2019-03-09 17:32:05 -0500

    Created Blog View

--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render,HttpResponse
+from .models import Blog
 
 def home(request):
-    context = {
-    'name':'guest',
-    }
+    blog = Blog.objects.all()
+    if blog is not None:
+        context = {
+        'blogs':blog,
+        }
+    else:
+        context={
+        'msg':"Sorry No Blogs Yet"
+        }
     return render(request, "home.html", context)
